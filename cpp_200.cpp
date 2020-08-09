b53e7bd91eec674462e14f9c4012214606142407
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-09 17:09:09 +0900

    // 012 - 주석 배우기 ( //, /* */ )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -2,23 +2,15 @@
 
 using namespace std;
 
-void Minus(const int x, const int y) {
-
-	cout << "x - y = " << x - y << endl;
-}
-
-int Plus(const int x, const int y) {
-
-	return x + y;
-}
-
 int main(int argc, char* argv[])
 {
-	Minus(10, 5);
-
-	cout << "x + y = " << Plus(2, 6) << endl;
+	//출력하지 않을 코드
+	//Minus(10, 5);
+	/*
+	cout << "x + y = " << Plus(2, 6) << endl; 
+	*/
 	
 	return 0;
 }
 
-// 011 - 함수 배우기 ( () )
+// 012 - 주석 배우기 ( //, /* */ )
