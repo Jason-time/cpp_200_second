ccdaa6597c94f82f69fb32d8fbfade50a1f53be5
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-09 17:02:45 +0900

    // 011 - 함수 배우기 ( () )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
+void Minus(const int x, const int y) {
+
+	cout << "x - y = " << x - y << endl;
+}
+
+int Plus(const int x, const int y) {
+
+	return x + y;
+}
+
 int main(int argc, char* argv[])
 {
-	const int kArraySize = 2;
+	Minus(10, 5);
 
-	int founding[kArraySize];
-	founding[0] = 918;
-	founding[1] = 1392;
-
-	cout << "고려 건국 연도 : " << founding[0] << endl;
-	cout << "조선 건국 연도 : " << founding[1] << endl;
-
+	cout << "x + y = " << Plus(2, 6) << endl;
+	
 	return 0;
 }
 
-// 010 - 배열 배우기 ( [크기] )
+// 011 - 함수 배우기 ( () )
