41605021242a1977ecffdec23315bcd642ca0113
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-09 15:56:15 +0900

    // 005 - 사칙연산 배우기 ( +, -, *, / )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,14 +1,23 @@
 ﻿#include <iostream>
+
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    const int GREATE_VICTORY_SALSU = 612;
-    const int GREATE_VICTORY_GWIJU = 1019;
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
 
-    cout << "고구려 살수대첩 연도 : " << GREATE_VICTORY_SALSU << "년" << endl;
-    cout << "고려 귀주대첩 연도 : " << GREATE_VICTORY_GWIJU << "년" << endl;
+    cout << "1 + 3 = " << one + three << endl;
+    cout << "4 - 2 = " << four - two << endl;
+    cout << "2 * 3 = " << two * three << endl;
+    cout << "4 / 2 = " << four / two << endl;
+
+    cout << "2 + 3 * 4 = " << two + three * four << endl;
+    cout << "1 + 4 / 2 = " << one + four / two << endl;
+
     return 0;
 }
 
-// 004 - 상수 배우기( const )
+// 005 - 사칙연산 배우기 ( +, -, *, / )
