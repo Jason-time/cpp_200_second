b2ec5cb19155114b99a35ae7e1078a22afbbe815
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-09 20:54:56 +0900

    // 028 - 비트 연산자 이해하기 ( &, ~, <<, >> ^ )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,16 +1,34 @@
 ﻿#include <iostream>
+#include <bitset>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int goguryeo = 37, baekjae = 18, silla = 57;
+	bitset<8> bit1;
+	bit1.reset(); // 0000 0000
+	bit1 = 127; // 0111 1111
 
-	printf("삼국 건국연도\n");
-	printf("고구려 bc%d년, 백제 bc%d년, 신라 bc%d년", goguryeo, baekjae, silla);
+	bitset<8> bit2;
+	bit2.reset();
+	bit2 = 0x20; //32
+
+	bitset<8> bit3 = bit1 & bit2;
+	bitset<8> bit4 = bit1 | bit2;
+	bitset<8> bit5 = bit1 ^ bit2;
+	bitset<8> bit6 = ~bit1;
+	bitset<8> bit7 = bit2 << 1;
+	bitset<8> bit8 = bit2 >> 1;
+
+	cout << "bit1 & bit2 : " << bit3 << ", " << bit3.to_ulong() << endl;
+	cout << "bit1 | bit2 : " << bit4 << ", " << bit4.to_ulong() << endl;
+	cout << "bit1 ^ bit2 : " << bit5 << ", " << bit5.to_ulong() << endl;
+	cout << "~bit1 : " << bit6 << ", " << bit6.to_ulong() << endl;
+	cout << "bit2 << 1 : " << bit7 << ", " << bit7.to_ulong() << endl;
+	cout << "bit2 >> 1 : " << bit8 << ", " << bit8.to_ulong() << endl;
 
 	return 0;
 }
 
-// 027 - 쉼표 연산자 이해하기 (,)
+// 028 - 비트 연산자 이해하기 ( &, ~, <<, >> ^ )
 
