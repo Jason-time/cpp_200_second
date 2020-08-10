59431ef18d2c3bbc96a6a3ac4f1e62c60b93c6a3
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-10 21:52:14 +0900

    // 035 - 순환문으로 특정 문자 개수 구하기(for)

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,22 +1,27 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int number = 7;
+	string str = "I'm a boy. You are a girl.";
 
-	for (int i = 0;i < 10; i++) {
+	char find = 'a';
 
-		if (i % 3 == 0)
-			continue;
-		else if (i == number)
-			break;
-		else
-			cout << "현재 i 값 : " << i << endl;
+	int size = str.size();
+	int count = 0;
+
+	for (int i = 0;i < size;i++) {
+
+		if (str[i] == find)
+			count++;
 	}
+
+	cout << "문장의 a 개수는 " << count << "개 입니다." << endl;
+
 	return 0;
 }
 
-// 034 - 중첩 순환문 이해하기 ( for~continue~break )
+// 035 - 순환문으로 특정 문자 개수 구하기(for)
 
