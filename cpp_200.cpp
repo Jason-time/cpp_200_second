4fe7b7b22731b30949c9b62fc33ae91da8a30b37
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-11 20:34:38 +0900

    // 067 - 숫자를 문자열로 변환하기 ( to_string )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,18 +1,20 @@
 ﻿#include <iostream>
-#include <algorithm>
 #include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string str = "gfedcba";
+	int number1 = 10;
+	double number2 = 67.8;
 
-	reverse(str.begin(), str.end());
+	string no_str1 = to_string(number1);
+	string no_str2 = to_string(number2);
 
-	cout << str << endl;
+	cout << "number1 : " << number1 << endl;
+	cout << "number2 : " << number2 << endl;
 
 	return 0;
 }
 
-// 066 - 문자열 뒤집기 ( reverse ) 
+// 067 - 숫자를 문자열로 변환하기 ( to_string )
