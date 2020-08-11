cceadbc71ab9548f22500cea9764ac4157d72057
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-11 20:31:27 +0900

    // 066 - 문자열 뒤집기 ( reverse )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string sort_str1 = "gojoeon";
-	string sort_str2 = "AaBbCcDdEe";
+	string str = "gfedcba";
 
-	sort(sort_str1.begin(), sort_str1.end());
-	sort(sort_str2.begin(), sort_str2.end());
+	reverse(str.begin(), str.end());
 
-	cout << "소문자만 정렬 : " << sort_str1 << endl;
-	cout << "대소문자만 정렬 : " << sort_str2 << endl;
+	cout << str << endl;
 
 	return 0;
 }
 
-// 065 - 문자열 정렬하기 ( sort )
+// 066 - 문자열 뒤집기 ( reverse ) 
