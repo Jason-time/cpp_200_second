3cfc3ec0a1043bd5a015425d4b056572e72045fe
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-11 14:52:13 +0900

    // 045 - 절대값과 제곱수 구하기 ( abs, fabs, pow )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << "round(1.1) : "<<round(1.1) << endl;
-	cout << "round(-2.3) : "<<round(-2.3) << endl;
-	cout << "round(40.5) : "<<round(40.5) << endl;
-	cout << "round(55.7) : "<<round(55.7) << endl;
-	cout << "round(100.9) : "<<round(100.9) << endl;
+	cout << "==절대값 구하기==" << endl;
+	cout << "-10의 절대값  " << abs(-10) << endl;
+	cout << "-5.72의 절대값 " << fabs(-5.72) << endl;
+	cout << endl;
+
+	cout << "==제곱수 구하기==" << endl;
+	cout << "2의 2승 : " << pow(2, 2) << endl;
+	cout << "3의 4승 : " << pow(3, 4) << endl;
+	
 	return 0;
 }
 
-// 044 - 실수 반올림하기 ( round )
+// 045 - 절대값과 제곱수 구하기 ( abs, fabs, pow )
