fb29d12431daf8ba7a86311888fef4d302bd1d79
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-11 12:51:28 +0900

    // 040 - 반복문을 이용하여 구구단 (15단) 출력하기

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -4,18 +4,27 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	int number = 0;
+	int offset = 0;
 
-	do {
-		number++;
+	for (int i = 1;i <= 15; i += offset) {
 
-		if (number % 3 == 0)
-			continue;
-		else
-			cout << "number 값 : " << number << endl;
-	} while (number < 10);
+		for (int j = 1;j <= 9;j++) {
 
+			cout << i << " * " << j << " = " << i * j;
+			for (int k = 0; k < offset;k++) {
+
+				cout << "\t";
+				cout << (i + k + 1) << " * " << j << " = " << (i + k + 1) * j;
+			}
+
+			cout << endl;
+		}
+
+		cout << endl;
+
+		offset++;
+	}
 	return 0;
 }
 
-// 039 - 조건 순환문 이해하기 ( do~while~continue~break )
+// 040 - 반복문을 이용하여 구구단 (15단) 출력하기
