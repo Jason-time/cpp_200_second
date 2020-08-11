28b3faefe0a7d0e60f42651323d10ecb275e541a
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-11 17:00:53 +0900

    // 056 - 문자열 대소문자 변환하기 ( toupper, tolower )

--- cpp_200.cpp
+++ cpp_200.cpp
@@ -1,14 +1,25 @@
 ﻿#include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	string baekhap_war = "백합벌 전투 : 백제 왕자 부여창의 일기토로 유명한 전투";
+	string silla = "divided into the Three  Kingdoms.";
 
-		cout << "백합벌 전투 설명 길이 : " <<baekhap_war.length()<< endl;
+	transform(silla.begin(), silla.end(), silla.begin(), toupper);
+
+	char lower_ch = 'g';
+	char upper_ch = 'B';
+
+	lower_ch = toupper(lower_ch);
+	upper_ch = tolower(upper_ch);
+
+	cout << "문자열 대문자로 : " << silla << endl;
+	cout << "문자를 대문자로 : " << lower_ch << endl;
+	cout << "문자를 소문자로 : " << upper_ch << endl;
 	return 0;
 }
 
-// 055 - 문자열 길이 구하기 ( length )
+// 056 - 문자열 대소문자 변환하기 ( toupper, tolower ) 
